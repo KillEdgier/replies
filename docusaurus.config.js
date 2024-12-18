7374f39912fc51e4c015749f28887fc656982d2c
Kell Sanders <Kell.Sanders@target.com>
2024-12-18 12:22:36 -0600

    futzing with config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,37 +1,25 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: ' ',
-  tagline: 'Dinosaurs are cool',
+  title: 'Replies',
+  tagline: 'Goofing with scammers',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://killedgier.github.io',
+  url: 'https://killedgier.github.io', // Set the production url of your site here
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  baseUrl: '/replies/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KillEdgier', // Usually your GitHub org/user name.
   projectName: 'replies', // Usually your repo name.
   deploymentBranch: 'main',
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
